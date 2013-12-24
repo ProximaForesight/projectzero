599f91b4dba66fb05872a61e659079cc4e521fe6
ProximaForesight <info@proxima.co.nz>
2013-12-24 16:43:29 +1300

    sCreated app for Project Zero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectzero::Application.config.secret_key_base = '2aa88e3eb7e1f6e58cdeefc25d9b1451e7d569d28d556d9996cdf7f025aa943b7739c82b7d83b7659b1a739f9ceee996a2953ac43b339864a809426a0dd25812'
